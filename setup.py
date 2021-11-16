8f162bb1522f22e71e93c96a19b74906a9fce4b5
Ram <heath.raman@gmail.com>
2021-11-16 20:20:52 +0000

    adding the patterns dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ setup(
     author='Ram',
     author_email='heath.raman@gmail.com',
     license='MIT',
-    install_requires=[],
+    install_requires=[
+        "patterns@git+https://github.com/raman4793/patterns.git@develop#egg=patterns"
+    ],
     packages=['commander'],
     package_data={},
     zip_safe=False
